4856e63e5a4e8ca4fef12e1214a8125fc6cc3bf3
Barbara Wolff Dick <me@barbarawolffdick.com>
2018-11-12 20:31:24 -0200

    First commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var rankingRouter = require('./routes/ranking');
+var comparativosRouter = require('./routes/comparativos');
+var disparosRouter = require('./routes/disparos');
+var pessoasRouter = require('./routes/pessoas');
+
+
+var app = express();
+
+// view engine setup
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/ranking', rankingRouter);
+app.use('/comparativos', comparativosRouter);
+app.use('/disparos', disparosRouter);
+app.use('/pessoas', pessoasRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,214 @@
+var express = require('express');
+var router = express.Router();
+var parse = require('csv-parse');
+var fs = require('fs');
+
+function csvRead(filename, fn) {
+  fs.readFile('./public/javascripts/csv/' + filename + '.csv', 'utf8', function(err, contents) {
+    const output = []
+    parse(contents, {
+      columns: true,
+      skip_empty_lines: true,
+    })
+    .on('readable', function(){
+      let record
+      while (record = this.read()) {
+        output.push(record)
+      }
+    })
+    .on('end', function() {
+      fn(output)
+    })
+  });
+}
+
+/* Função de ranking em BAIRROS na região metropolitana */
+router.get('/bairros', function(req, res, next) {
+  csvRead('rbairros_rm', function(data) {
+    const output = { labels: [], values: [] }
+
+    var disparos = data
+    .map(function(el){ // transforma disparos em Int (é string)
+      el.disparos = parseInt(el.disparos)
+      return el
+    })
+    .sort(function(a, b) { // ordena por qtd de disparos
+      return b.disparos - a.disparos
+    })
+    .slice(0, 10) // pega os 5 primeiros
+    .forEach(function(el){ // adiciona bairros e disparos no objeto output
+      output.labels.push(el.bairros)
+      output.values.push(el.disparos)
+    }, this)
+
+    res.send(output) // envia o objeto final
+  })
+});
+
+/* Função de ranking em MUNICIPIOS na região metropolitana */
+router.get('/municipios', function(req, res, next) {
+  csvRead('rmunicipios', function(data) {
+    const output = { labels: [], values: [] }
+
+    var disparos = data
+    .map(function(el){ // transforma disparos em Int (é string)
+      el.disparos = parseInt(el.disparos)
+      return el
+    })
+    .sort(function(a, b) { // ordena por qtd de disparos
+      return b.disparos - a.disparos
+    })
+    .slice(0, 10) // pega os 5 primeiros
+    .forEach(function(el){ // adiciona bairros e disparos no objeto output
+      output.labels.push(el.municipios)
+      output.values.push(el.disparos)
+    }, this)
+
+    res.send(output) // envia o objeto final
+  })
+});
+
+/* Função de ranking em BELFORD ROXO na região metropolitana */
+router.get('/broxo', function(req, res, next) {
+  csvRead('rbelfordroxo', function(data) {
+    const output = { labels: [], values: [] }
+
+    var disparos = data
+    .map(function(el){ // transforma disparos em Int (é string)
+      el.disparos = parseInt(el.disparos)
+      return el
+    })
+    .sort(function(a, b) { // ordena por qtd de disparos
+      return b.disparos - a.disparos
+    })
+    .slice(0, 10) // pega os 5 primeiros
+    .forEach(function(el){ // adiciona bairros e disparos no objeto output
+      output.labels.push(el.bairros)
+      output.values.push(el.disparos)
+    }, this)
+
+    res.send(output) // envia o objeto final
+  })
+});
+
+
+/* Função de ranking em SAO GONÇALO na região metropolitana */
+router.get('/saogoncalo', function(req, res, next) {
+  csvRead('rsaogoncalo', function(data) {
+    const output = { labels: [], values: [] }
+
+    var disparos = data
+    .map(function(el){ // transforma disparos em Int (é string)
+      el.disparos = parseInt(el.disparos)
+      return el
+    })
+    .sort(function(a, b) { // ordena por qtd de disparos
+      return b.disparos - a.disparos
+    })
+    .slice(0, 10) // pega os 5 primeiros
+    .forEach(function(el){ // adiciona bairros e disparos no objeto output
+      output.labels.push(el.bairros)
+      output.values.push(el.disparos)
+    }, this)
+
+    res.send(output) // envia o objeto final
+  })
+});
+
+/* Função de ranking em UPP na região metropolitana */
+router.get('/upp', function(req, res, next) {
+  csvRead('rupp', function(data) {
+    const output = { labels: [], values: [] }
+
+    var disparos = data
+    .map(function(el){ // transforma disparos em Int (é string)
+      el.disparos = parseInt(el.disparos)
+      return el
+    })
+    .sort(function(a, b) { // ordena por qtd de disparos
+      return b.disparos - a.disparos
+    })
+    .slice(0, 10) // pega os 5 primeiros
+    .forEach(function(el){ // adiciona bairros e disparos no objeto output
+      output.labels.push(el.bairros)
+      output.values.push(el.disparos)
+    }, this)
+
+    res.send(output) // envia o objeto final
+  })
+});
+
+
+
+/* Spider chart agentes de segurança */
+router.get('/agentesmortos', function(req, res, next) {
+  csvRead('ag_seg', function(data) {
+    const output = { labels: [], values: [] }
+    const group = {}
+
+    data.forEach(function(el){ // transforma disparos em Int (é string)
+      if (parseInt(el.mortos) != 0) {
+        if (group[el.motivo] == null) group[el.motivo] = 0
+        group[el.motivo] = group[el.motivo] + parseInt(el.mortos)
+        return el
+      }
+    })
+
+    for (key in group) {
+      output.labels.push(key)
+      output.values.push(group[key])
+    }
+
+    res.send(output) // envia o objeto final
+  })
+});
+
+/* Spider chart regioes mortes chacinas*/
+router.get('/chacinas', function(req, res, next) {
+  csvRead('chacinas', function(data) {
+    const output = { labels: [], values: [] }
+    const group = {}
+
+    data.forEach(function(el){ // transforma disparos em Int (é string)
+      if (parseInt(el.mortos) != 0) {
+        if (group[el.municipio] == null) group[el.municipio] = 0
+        group[el.municipio] = group[el.municipio] + parseInt(el.mortos)
+        return el
+      }
+    })
+
+    for (key in group) {
+      output.labels.push(key)
+      output.values.push(group[key])
+    }
+
+    res.send(output) // envia o objeto final
+  })
+});
+
+
+/* Spider chart regioes mortes chacinas*/
+router.get('/bala', function(req, res, next) {
+  csvRead('bala_perdida', function(data) {
+    const output = { labels: [], values: [] }
+    const group = {}
+
+    data.forEach(function(el){ // transforma disparos em Int (é string)
+      if (parseInt(el.mortos) != 0) {
+        if (group[el.municipio] == null) group[el.municipio] = 0
+        group[el.municipio] = group[el.municipio] + parseInt(el.mortos)
+        return el
+      }
+    })
+
+    for (key in group) {
+      output.labels.push(key)
+      output.values.push(group[key])
+    }
+
+    res.send(output) // envia o objeto final
+  })
+});
+
+
+module.exports = router;
